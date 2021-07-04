90337bde784cdf113f0e7dd25698ce17967124d1
begimai.murzalieva@gmail.com <begimai.murzalieva@gmail.com>
2021-07-04 14:36:10 +0200

    DB zaprosy

---
+++ Begimai Murzalieva /zaprosy.sql
@@ -0,0 +1,28 @@
+-- 1 zadanie
+
+SELECT * FROM Payments
+WHERE  PersonalAccount = 996770394000 AND PaymentAmount >= 10
+
+-- 2 zadanie
+
+--SELECT * FROM Payments
+--WHERE PersonalAccount = 996777394000 OR PaymentAmount > 60
+
+UPDATE Payments
+SET PersonalAccount = 996550555055
+WHERE PersonalAccount = 996777394000 OR PaymentAmount > 60
+
+-- 3 zadanie 
+
+
+SELECT * FROM Services 
+WHERE MaxAmount <= 5000 AND OperatorName = 'EwalletFO'
+
+-- 4 zadanie 
+
+INSERT INTO EncashmentInfo ( id , PartnerID, EnDate, EnAmount, IsOnline ) VALUES ( 1 , 1122, '2021-07-20', 5000, 100)
+
+-- 5 zadanie 
+
+DELETE FROM Services
+WHERE OperatorName = 'InternetOperator'
